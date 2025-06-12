018aeb17d1e8250e84f22ecda46de498c4f4eadb
Gergana Ivanova <gslavcheva933@gmail.com>
2025-06-12 10:31:09 +0300

    Add files via upload

---
+++ Test-Gergana 12.06.2025/BankAccount.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Gergana_12._06._2025
+{
+    internal class BankAccount
+    {
+    }
+}
---
+++ Test-Gergana 12.06.2025/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+namespace Test_Gergana_12._06._2025
+{
+    internal class Program
+    {
+        public class BankAccount
+        {
+            public int ID;
+            private double Balance;
+
+            public void Deposit(double amount)
+            {
+                Balance += amount;
+            }
+            public void Withdraw(double amount)
+            {
+                Balance -= amount;
+            }
+            public override string ToString()
+            {
+                return $"Account ID: {ID}, balance: {Balance:F2}";
+            }
+
+
+            static void Main(string[] args)
+            {
+                BankAccount acc = new BankAccount();
+                acc.ID = 1;
+                acc.Deposit(15);
+                acc.Withdraw(5);
+
+                System.Console.WriteLine(acc.ToString());
+            }
+        }
+    }
+}
